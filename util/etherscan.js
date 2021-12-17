54acd9bfe1eb935c683fd9abb893f3c29575ac7b
thefattestcat <a001gtb@gmail.com>
2021-12-18 12:50:36 +1300

    Etherscan scraper

---
+++ util/etherscan.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const fs = require("fs");
+const APIKEY = "ZYCPMJ3CQZMESWQ2T9MWQJ7BT2JY47R21D";
+const decoder = require('ethereum-input-data-decoder-without-fs');
+
+(async () => {
+  const baseURL = "https://www.cyberkongz.com/api/metadata";
+  const CONTRACT_ADDR = "0x57a204AA1042f6E66DD7730813f4024114d74f37";
+  const tokenData = await axios.get(
+    `https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=${CONTRACT_ADDR}&apikey=${APIKEY}`
+  );
+  console.log(tokenData.data)
+  const AMOUNT = tokenData?.data?.result || null;
+  console.log(`Total tokens: ${AMOUNT}`);
+  //const ws = fs.createWriteStream('.')
+  for (let i = 0; i <= Number(AMOUNT); i++) {
+    const res = await axios.get(`${baseURL}/${i}`);
+    const data = res.data;
+    console.log(`[${i}] ${data.name}: ${data.image}`);
+    await sleep(1000);
+  }
+})();
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
